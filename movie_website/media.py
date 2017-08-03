0add80d06d1b3eeec51c01fa787e30cec7115373
Ney Torres <Neyt@users.noreply.github.com>
2017-08-03 15:12:13 -0500

    adding __doc__ comments
    It is considered good practice to provide enough information to the user about the class you're implementing.
    You have the option to use docstring. Docstring can be served as documentation for the user,
    by accessing the __doc__ attribute every class natively possess or call the builtin help() function.
    If you want to learn more about docstring follow these links:
    http://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_google.html
    or:
    http://epydoc.sourceforge.net/docstrings.html
    https://www.python.org/dev/peps/pep-0257/#multi-line-docstrings

--- movie_website/media.py
+++ movie_website/media.py
@@ -9,9 +9,13 @@ gives you practice importing Python files.
 import webbrowser
 
 class Movie():
-    # This class provides a way to store movie related information
+    """
+    This class provides a way to store movie related information
+    """
     def __init__(self, title, poster_image_url, trailer_youtube_url):
-        # initialize instance of class Movie
+        """
+        initialize instance of class Movie
+        """
         self.title = title
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
