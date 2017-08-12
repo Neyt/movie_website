34f79f13fe56795b46ee42b3a498fd66f8989d7a
Ney Torres <Neyt@users.noreply.github.com>
2017-08-11 21:44:10 -0500

    make lines shorter with \
    http://pep8online.com/checkresult

--- movie_website/entertainment_center.py
+++ movie_website/entertainment_center.py
@@ -3,23 +3,27 @@ Lesson 3.4: Make Classes
 Mini-Project: Movies Website
 In this file, you will define instances of the class Movie defined in media.py.
 After you follow along with Kunal, make some instances of your own!
-After you run this code, open the file fresh_tomatoes.html to
-see your webpage!
+After you run this code, open the file fresh_tomatoes.html
+to see your webpage!
 '''
 
 import media
 import fresh_tomatoes
 
 Jiro = media.Movie("Jiro Dreams of Sushi",
-                   "http://ecx.images-amazon.com/images/I/810Fw8dOh4L._SL1500_.jpg",
+                   "http://ecx.images-amazon.com/images/\
+                    I/810Fw8dOh4L._SL1500_.jpg",
                    "https://www.youtube.com/watch?v=I1UDS2kgqY8")
 print (Jiro.trailer_youtube_url)
 TED_Talks = media.Movie("TED Talks: Life Hacks",
-                        "http://bingeout.com/wp-content/uploads/2015/06/FluidStance_Ted-Talks-689x400.png",
+                        "http://bingeout.com/wp-content/uploads/\
+                        2015/06/FluidStance_Ted-Talks-689x400.png",
                         "https://youtu.be/bhPfzTvqFTU")
 print (TED_Talks.trailer_youtube_url)
 Joan_Rivers = media.Movie("Joan Rivers: A Piece of Work",
-                          "http://www.chicagonow.com/blogs/life-and-times-of-a-young-republican/jriverspieceofworkposter.jpg",
+                          "http://www.chicagonow.com/blogs/\
+                          life-and-times-of-a-young-republican/\
+                          jriverspieceofworkposter.jpg",
                           "https://www.youtube.com/watch?v=cn7gAG5_tkM")
 print (Joan_Rivers.trailer_youtube_url)
 Enron = media.Movie("Enron: The Smartest Guys in the Room",
@@ -27,7 +31,8 @@ Enron = media.Movie("Enron: The Smartest Guys in the Room",
                     "https://youtu.be/-w6duQhWuVk")
 print (Enron.trailer_youtube_url)
 American_Experience = media.Movie("American Experience: Henry Ford",
-                                  "http://old.seattletimes.com/ABPub/2013/01/28/2020232995.jpg",
+                                  "http://old.seattletimes.com/ABPub/\
+                                  2013/01/28/2020232995.jpg",
                                   "https://youtu.be/smyMT2pnLbQ")
 print (American_Experience.trailer_youtube_url)
 movies = [Jiro, TED_Talks, Joan_Rivers, Enron, American_Experience]
